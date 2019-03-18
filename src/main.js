f41ca783001dcb19ed95b29cc5c137a6542301fa
qp97vi <1763069910@qq.com>
2019-03-18 19:35:29 +0800

    Revert "我的页面基本完成"
    This reverts commit 57c2b677f7b0ece4165f1e6f5713f41d0e5b35d6.

--- src/main.js
+++ src/main.js
@@ -1,29 +1,38 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import routes from './routers'
-import {store} from './store/store'
-
-import vAudio from '@/components/audio/audio'
-import '@/assets/css/reset.css'
-import '@/assets/css/basic.css'
-import '@/assets/js/iconfont.js'
+import App from './App'
+import router from './router'
+import './assets/font/iconfont.css'
 
 
 Vue.config.productionTip = false
+import axios from 'axios'
+import 'mint-ui/lib/style.css'
 
-Vue.use(VueRouter)
 
-const router = new VueRouter({
-  mode:'hash',
-  routes
-});
+import { Spinner ,InfiniteScroll,Swipe, SwipeItem} from 'mint-ui';
+import { Lazyload } from 'mint-ui';
 
+Vue.use(Lazyload);
+
+import store from './store/store';
+
+Vue.use(InfiniteScroll,Lazyload);
+
+
+Vue.component(Spinner.name, Spinner);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+import 'mint-ui/lib/style.css'
+axios.defaults.baseURL='http://192.168.1.28:3000/'
+Vue.prototype.axios = axios;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  store: store,
-  components: {
-    vAudio
-  }
+  store,
+  components: { App },
+  template: '<App/>'
 })
